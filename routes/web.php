30b235a06d1144c3aee3afa160e3619311667eab
Firman Situmorang <firmansitumorang@gmail.com>
2024-10-23 17:30:14 +0700

    utp

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user/profile', [UserController::class, 'profile']);
-Route::get('/user/create', [UserController::class,'create']);
-Route::post('/user/store', [UserController::class,'store'])->name('user.store');
+Route::get('/profile/{nama}/{kelas}/{npm}', [ProfileController::class, 'profile']);
+Route::get('/profile/{nama}/{nama_kelas}/{npm}', [UserController::class, 'profile']) -> name('profile');
+Route::get('/user/create', [UserController::class, 'create'])-> name("users.create");
+Route::post('/user/store', [UserController::class, 'store']) -> name('user.store');
+Route::get('/user', [UserController::class, 'index']) -> name('users.index');
+Route::get('show/{id}', [UserController::class, 'show']) ->name('users.show');
+Route::put('user/{id}', [UserController::class, 'update'])->name('user.update');
+Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
+Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
